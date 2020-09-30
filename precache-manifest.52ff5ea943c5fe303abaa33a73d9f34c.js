f6fc764f71dd5f2cf8ee7d429597bd39d39889b2
Ankit Allwin <56684502+ankitallwin@users.noreply.github.com>
2020-09-30 20:53:29 +0530

    Updates

---
+++ precache-manifest.52ff5ea943c5fe303abaa33a73d9f34c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e146269ec9d60170353fe490ebc71c8b",
+    "url": "/greeting-reactJS/index.html"
+  },
+  {
+    "revision": "40aebb71768fb355e809",
+    "url": "/greeting-reactJS/static/css/main.dd7fa2ca.chunk.css"
+  },
+  {
+    "revision": "6d5b93a808562d051c29",
+    "url": "/greeting-reactJS/static/js/2.5281b13a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/greeting-reactJS/static/js/2.5281b13a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40aebb71768fb355e809",
+    "url": "/greeting-reactJS/static/js/main.e0d3d4f7.chunk.js"
+  },
+  {
+    "revision": "41da27de1ece8d09806e",
+    "url": "/greeting-reactJS/static/js/runtime-main.ee1f4e45.js"
+  }
+]);
